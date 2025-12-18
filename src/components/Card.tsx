ea51baf12625db9614d3c912bc5e86e3b1d53f9e
Joanna Erika Koch <roijohnkoch@gmail.com>
2025-12-18 14:27:17 +0800

    Commit for adding Link for navigation on click game cards.

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { CardProps } from "@/lib/types";
 import styles from "./Card.module.scss";
 
@@ -28,21 +29,27 @@ const Card: React.FC<CardProps> = (props) => {
       itemScope
       itemType="http://schema.org/VideoGame"
     >
-      <div className={styles.poster}>
-        <Image 
-          src={`https://picsum.photos/seed/${encodeURIComponent(title + releaseDate)}/600/900`}
-          alt={`${title} Cover Image`}
-          fill
-          itemProp="image"
-          priority={false}
-        />
-      </div>
-      <div className={styles.content}>
-        <h3>{title}</h3>
-        <span>Release Date: <time dateTime={releaseDate} itemProp="releaseDate">{formatUnixDate(releaseDate)}</time></span>
-        <span>Genre: {genre.genreName}</span>
-        <span>Average Rating: {averageRating}</span>
-      </div>
+      <Link
+        href={`/games/${gameDetails.id}`}
+        prefetch={false}
+      >
+        <div className={styles.poster}>
+          <Image 
+            src={`https://picsum.photos/seed/${encodeURIComponent(title + releaseDate)}/600/900`}
+            alt={`${title} Cover Image`}
+            fill
+            itemProp="image"
+            sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
+            loading="eager"
+          />
+        </div>
+        <div className={styles.content}>
+          <h3>{title}</h3>
+          <span>Release Date: <time dateTime={releaseDate} itemProp="releaseDate">{formatUnixDate(releaseDate)}</time></span>
+          <span>Genre: {genre.genreName}</span>
+          <span>Average Rating: {averageRating}</span>
+        </div>
+      </Link>
     </article>
   );
 }
